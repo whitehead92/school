cea840efd91b1f76be2ec5d42a2dd0e20a38026d
whitehead <sylk_92@mail.ru>
2024-05-12 14:38:40 +1000

    Started to configure main layout

--- frontend/src/layouts/default.vue
+++ frontend/src/layouts/default.vue
@@ -1,12 +1,12 @@
 <template>
   <v-app>
-    <DefaultBar />
+    <DefaultNav />
 
     <DefaultView />
   </v-app>
 </template>
 
 <script lang="ts" setup>
-  import DefaultBar from './default/AppBar.vue'
+  import DefaultNav from './default/Nav.vue'
   import DefaultView from './default/View.vue'
 </script>
---
+++ frontend/src/layouts/default/Nav.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-navigation-drawer>
+    <v-list>
+      <v-list-item title="School" subtitle="navigation"></v-list-item>
+      <v-divider></v-divider>
+      <v-list-item link title="Home" to="/"></v-list-item>
+      <v-list-item link title="API (not implemented)" href="/api/disabled"></v-list-item>
+      <v-list-item link title="Admin site" href="/admin/"></v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script lang="ts" setup>
+  //
+</script>
