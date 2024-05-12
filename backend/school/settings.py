9edbe2832f6a60a129b55897ec518c2b0be7ed93
whitehead <sylk_92@mail.ru>
2024-05-12 13:13:08 +1000

    Serving both front and back Separate urls for admin, api and static

--- backend/school/settings.py
+++ backend/school/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from os import getenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,10 @@ SECRET_KEY = 'django-insecure-heou-o+s3+um8w5tc^)5k(+g*uqi6yr963%tl6zvdg3-2pz*-i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    getenv('EXTERNAL_HOST', 'localhost'),
+    'backend-dev',
+]
 
 
 # Application definition
@@ -116,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
